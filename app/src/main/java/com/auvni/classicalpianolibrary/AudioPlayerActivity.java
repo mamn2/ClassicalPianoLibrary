9c56a81e433fd4841626153bcf903517880022be
Mohamed Amn <mamn2@illinois.edu>
2018-11-26 22:54:39 -0600

    Got the audio player class to accept MP3 URLs

--- app/src/main/java/com/auvni/classicalpianolibrary/AudioPlayerActivity.java
+++ app/src/main/java/com/auvni/classicalpianolibrary/AudioPlayerActivity.java
@@ -1,34 +1,77 @@
 package com.auvni.classicalpianolibrary;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URI;
 
 public class AudioPlayerActivity extends AppCompatActivity {
 
+    String url = "https://www.searchgurbani.com/audio/sggs/1.mp3";
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
     }
 
+    /*public void setThisURL(String url) {
+
+    }*/
+
     /**
      * This method is called when a user clicks the play button.
-     * @param v
+     * @param v xml file button
      */
-    public void play(View v) {}
+    public void play(View v) {
+        if (player == null) {
+            try {
+                player = new MediaPlayer();
+                player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                player.setDataSource(url);
+                player.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Input does not exist", Toast.LENGTH_SHORT).show();
+            }
+        }
+        player.start();
+    }
 
     /**
      * This method is called when a user clicks the pause button.
-     * @param v
+     * @param v xml file button
      */
-    public void pause(View v) {}
+    public void pause(View v) {
+        if (player != null) {
+            player.pause();
+        }
+    }
 
     /**
      * This method is called when a user want to stop the song all together.
-     * @param v
+     * @param v xml file button
      */
-    public void stop(View v) {}
+    public void stop(View v) {
+        stopPlayer();
+    }
 
+    public void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+            Toast.makeText(this, "MediaPlayer has been released", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        stopPlayer();
+    }
 }
