02b58542273fb82ad3642a970f9ec77efa7d00dd
Mohamed Amn <mamn2@illinois.edu>
2018-12-02 16:57:10 -0600

    Merged the adapter to the search results activity

--- app/src/main/java/com/auvni/classicalpianolibrary/SearchResultsActivity.java
+++ app/src/main/java/com/auvni/classicalpianolibrary/SearchResultsActivity.java
@@ -1,7 +1,10 @@
 package com.auvni.classicalpianolibrary;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.SeekBar;
 
@@ -14,6 +17,7 @@ public class SearchResultsActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     SeekBar seekbar;
     SongRecyclerListAdapter songRecyclerListAdapter;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +26,9 @@ public class SearchResultsActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
         songRecyclerListAdapter = new SongRecyclerListAdapter(this, trackInfoArrayList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setAdapter(songRecyclerListAdapter);
     }
 }
