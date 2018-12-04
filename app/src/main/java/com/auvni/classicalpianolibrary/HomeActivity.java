ad67237e8fbcdea3c7f9ed0f89967d4d9bf2da9b
Mohamed Amn <mamn2@illinois.edu>
2018-12-03 18:06:54 -0600

    Search algorithm works

--- app/src/main/java/com/auvni/classicalpianolibrary/HomeActivity.java
+++ app/src/main/java/com/auvni/classicalpianolibrary/HomeActivity.java
@@ -1,24 +1,21 @@
 package com.auvni.classicalpianolibrary;
 
+import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
-
+    private String songNames;
+    ArrayList<TrackInfo> trackInfoArrayList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,27 +28,12 @@ public class HomeActivity extends AppCompatActivity {
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, SearchResultsActivity.class));
+                Intent intent = new Intent(HomeActivity.this, SearchResultsActivity.class);
+                EditText text = (EditText) HomeActivity.this.findViewById(R.id.editText);
+                songNames = text.getText().toString();
+                intent.putExtra("key", songNames);
+                startActivity(intent);
             }
         });
     }
-
-
-    /*
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-
-    private void connected() {
-
-    }
-
-
-    protected void onStop() {
-        super.onStop();
-    }
-*/
-
-
 }
--- app/src/main/java/com/auvni/classicalpianolibrary/SearchResultsActivity.java
+++ app/src/main/java/com/auvni/classicalpianolibrary/SearchResultsActivity.java
@@ -3,17 +3,21 @@ package com.auvni.classicalpianolibrary;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
+import android.widget.Toast;
+
 import com.spotify.android.appremote.api.ConnectionParams;
 import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.PlayerApi;
@@ -35,17 +39,23 @@ public class SearchResultsActivity extends AppCompatActivity implements SpotifyR
     SongRecyclerListAdapter songRecyclerListAdapter;
     static MediaPlayer player;
     ImageButton fullScreen;
+    String getSongName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            getSongName = extras.getString("key");
+        }
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         seekbar = (SeekBar) findViewById(R.id.seekBar);
         fullScreen = (ImageButton) findViewById(R.id.fullScreenButton);
-
-        loadTracks();
+        loadSpotify();
+        loadAll();
+        search(getSongName);
 
         songRecyclerListAdapter = new SongRecyclerListAdapter(this, trackInfoArrayList);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
@@ -142,14 +152,30 @@ public class SearchResultsActivity extends AppCompatActivity implements SpotifyR
         return super.onOptionsItemSelected(item);
     }
 
-    private void loadTracks() {
+    private void search(String song) {
+        boolean found = false;
+        for (int i = 0; i < trackInfoArrayList.size(); i++) {
+            if (trackInfoArrayList.get(i).getSongName().equals(song)) {
+                TrackInfo info = trackInfoArrayList.get(i);
+                trackInfoArrayList = new ArrayList<>();
+                trackInfoArrayList.add(info);
+                found = true;
+            }
+        }
+        if (!found) {
+            setContentView(R.layout.no_songs);
+        }
+    }
+
+    private void loadAll() {
         TrackInfo track = new TrackInfo("Girls Like You", "Maroon 5", "https://fullgaana.in/siteuploads/files/sfd10/4966/Maroon%205%20-%20Girls%20Like%20You%20(feat.%20Cardi%20B)(FullGaana.In).mp3");
         trackInfoArrayList.add(track);
 
         TrackInfo track2 = new TrackInfo("Faded", "Alan Walker", "https://fullgaana.in/siteuploads/files/sfd6/2895/Faded%20(Alan%20Walker)(FullGaana.In).mp3");
         trackInfoArrayList.add(track2);
+    }
 
-
+    private void loadSpotify() {
 
         AuthenticationRequest.Builder builder =
                 new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
